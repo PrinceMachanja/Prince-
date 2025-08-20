a2077aee8b9240d9dc03e2774f3b1842170e5a45
PrinceMachanja <machanjap368@gmail.com>
2025-08-20 00:52:14 -0700

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,11 @@
+PI = 3.14159
+GRAVITY = 9.81
+radius = 5
+height = 10
+volume = PI * (radius ** 2) * height
+print("Radius", radius)
+print("height:",height)
+print("Volume of the cylinder:", volume)
+radius = 7
+new_volume = PI * (radius ** 2) * height
+print("Updated Volume with new radius:", new_volume)
